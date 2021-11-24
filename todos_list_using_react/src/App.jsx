b9ff5fa64fd391b21b0d38522a896a48016f8b77
unknown <aneeshsharmachd@gmail.com>
2021-11-24 02:55:13 -0500

    Added x button and made component of add function

--- todos_list_using_react/src/App.jsx
+++ todos_list_using_react/src/App.jsx
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
+import ToDoLists from './TodoLists';
 
 const App = () => {
 
     const  [inputList, setInputList] = useState(""); // hooks to add onChange of input text
     const [Items, setItems] = useState([]); // hooks array to store items on button click 
 
-    const itemEvents = (event) => {
+    const itemEvents = (event) => {        //displays inputed data in list
         setInputList(event.target.value);
     };
 
-    const listOfItems = (event) => {
+    const listOfItems = (event) => {    // adds all prev items to list and then the new item to it
         setItems((oldItems) => {
             return [...oldItems, inputList];
         });
+        setInputList(""); // to empty the input field after addition
     };
 
     return (
@@ -22,15 +24,17 @@ const App = () => {
                     <br/>
                     <h1>ToDo List</h1>
                     <br/>
-                    <input type= "text" placeholder = "Add an Item" onChange={itemEvents}/>
+                    <input type= "text" placeholder = "Add an Item" 
+                    value={inputList}    onChange={itemEvents}/> 
                     <button onClick={listOfItems}> + </button>
 
                     <ol>
                         {/* <li> {inputList} </li> */}
                         
-                        {
-                            Items.map( (itemval) => {
-                                return <li>{itemval}</li>
+
+                        {   // add itemval to map                                 
+                            Items.map( (itemval) => {   //calls the component ToDoLsits
+                                return < ToDoLists text = {itemval} />;
                             })
                         }
                     
---
+++ todos_list_using_react/src/TodoLists.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToDoLists = (props) => { // using props text = itemval and text gets added to array
+    return (<>
+
+        <div className="todo_style">
+        <span className="fa fa-times">x</span>
+        <li>{props.text}</li>
+        </div>
+
+    </>);
+
+}
+
+export default ToDoLists;
