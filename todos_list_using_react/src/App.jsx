ee623fd2ef136b06fef9dd4df26ab76b5f612873
unknown <aneeshsharmachd@gmail.com>
2021-11-24 02:19:44 -0500

    Implemented addition of items

--- todos_list_using_react/src/App.jsx
+++ todos_list_using_react/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+
+    const  [inputList, setInputList] = useState(""); // hooks to add onChange of input text
+    const [Items, setItems] = useState([]); // hooks array to store items on button click 
+
+    const itemEvents = (event) => {
+        setInputList(event.target.value);
+    };
+
+    const listOfItems = (event) => {
+        setItems((oldItems) => {
+            return [...oldItems, inputList];
+        });
+    };
+
     return (
         <>
             <div className="main_div">
@@ -8,12 +22,18 @@ const App = () => {
                     <br/>
                     <h1>ToDo List</h1>
                     <br/>
-                    <input type= "text" placeholder = "Add an Item"/>
-                    <button> + </button>
+                    <input type= "text" placeholder = "Add an Item" onChange={itemEvents}/>
+                    <button onClick={listOfItems}> + </button>
 
                     <ol>
-                        <li> Buy Apples </li>
-                        <li> Buy Eggs </li>
+                        {/* <li> {inputList} </li> */}
+                        
+                        {
+                            Items.map( (itemval) => {
+                                return <li>{itemval}</li>
+                            })
+                        }
+                    
                     </ol>
                 </div>
             </div>
