3503dcd7a187f0c04a97f650d98ed1c0830bbae3
unknown <aneeshsharmachd@gmail.com>
2021-11-24 03:28:30 -0500

    Deletion implemented

--- todos_list_using_react/src/App.jsx
+++ todos_list_using_react/src/App.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ToDoLists from './TodoLists';
 
 const App = () => {
 
-    const  [inputList, setInputList] = useState(""); // hooks to add onChange of input text
+    const [inputList, setInputList] = useState(""); // hooks to add onChange of input text
     const [Items, setItems] = useState([]); // hooks array to store items on button click 
 
     const itemEvents = (event) => {        //displays inputed data in list
@@ -17,27 +17,42 @@ const App = () => {
         setInputList(""); // to empty the input field after addition
     };
 
+    const deleteItems = (id) => {
+        console.log("deleted");
+        // delete the id indexed element of array 
+        setItems((oldItems) => {
+            return oldItems.filter((arrElem, index) => { return index !== id; });
+        });
+    };
+
     return (
         <>
             <div className="main_div">
                 <div className="center_div">
-                    <br/>
+                    <br />
                     <h1>ToDo List</h1>
-                    <br/>
-                    <input type= "text" placeholder = "Add an Item" 
-                    value={inputList}    onChange={itemEvents}/> 
+                    <br />
+                    <input type="text" placeholder="Add an Item"
+                        value={inputList} onChange={itemEvents} />
                     <button onClick={listOfItems}> + </button>
 
                     <ol>
                         {/* <li> {inputList} </li> */}
-                        
+
 
                         {   // add itemval to map                                 
-                            Items.map( (itemval) => {   //calls the component ToDoLsits
-                                return < ToDoLists text = {itemval} />;
+                            Items.map((itemval, index) => {   //calls the component ToDoLsits
+                                return < ToDoLists
+                                    key={index}
+                                    id={index}
+                                    text={itemval} // 3 prpos used
+                                    onSelect={deleteItems}
+
+                                     />;
+                                    
                             })
                         }
-                    
+
                     </ol>
                 </div>
             </div>
--- todos_list_using_react/src/TodoLists.jsx
+++ todos_list_using_react/src/TodoLists.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const ToDoLists = (props) => { // using props text = itemval and text gets added to array
-    return (<>
 
-        <div className="todo_style">
-        <span className="fa fa-times">x</span>
-        <li>{props.text}</li>
-        </div>
 
-    </>);
 
+    return (
+        <>
+            <div className="todo_style">
+                <span className="fa fa-times"
+                    onClick={() => {
+                        props.onSelect(props.id);
+                    }}>x</span>
+                <li>{props.text}</li>
+            </div>
+        </>
+    );
 }
 
 export default ToDoLists;
